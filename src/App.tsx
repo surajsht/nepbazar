258f015955350495d49dbaeb212670e5b6105da8
suraj <surajsht2055@gmail.com>
2026-05-29 13:21:01 +0545

    working on navbar ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from "./component/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="font-sans">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,43 @@
+import { IoCartOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="hidden lg:block container mx-auto px-4">
+      <div className="flex items-center justify-between py-6">
+        <h1 className="font-display font-semibold text-2xl"> NepBazar </h1>
+
+        <ul className="flex items-center justify-between gap-8">
+          <li> Home </li>
+          <li> Shop </li>
+          <li> Men </li>
+          <li> Women </li>
+          <li> New Arrival </li>
+          <li> Deals </li>
+        </ul>
+
+        <div className="flex items-center justify-between gap-4">
+          <div className="relative">
+            <FaRegHeart size={18} />
+            <span className="absolute top-[-12px] right-[-8px] h-4 w-4 bg-black text-white rounded-full flex items-center justify-center text-xs">
+              0
+            </span>
+          </div>
+
+          <div className="relative">
+            <IoCartOutline size={22} />
+            <span className="absolute top-[-12px] right-[-8px] h-4 w-4 bg-black text-white rounded-full flex items-center justify-center text-xs">
+              0
+            </span>
+          </div>
+
+          <button className="bg-black text-white rounded-full py-2 px-6 font-semibold">
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
